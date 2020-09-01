072271cb7a030e3b4856176b8915bea52f23473b
akshay-varun <akshaygr4@gmail.com>
2020-09-01 22:21:20 +0530

    Added Registration.

---
+++ public/register.php
@@ -0,0 +1,29 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+  <div class="header">
+  	<h2>Register</h2>
+  </div>
+
+  <form method="post" action="register.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  	  <label>Username</label>
+  	  <input type="text" name="username" value="<?php echo $username; ?>">
+  	</div>
+      <br>
+      <div class="input-group">
+          <button type="submit" class="btn" name="reg_user">Register</button>
+      </div>
+      <br>
+  	<p>
+  		Already a member? <a href="index.php">Sign in</a>
+  	</p>
+  </form>
+</body>
+</html>
---
+++ public/server.php
@@ -0,0 +1,110 @@
+<?php
+session_start();
+
+
+$username = "";
+$topic="";
+$textareaValue="";
+$percentage="";
+$errors = array();
+
+$db = mysqli_connect('localhost', 'feynman', 'fem&65n$78@aIOPu*j!248', 'feynman');
+
+
+if (isset($_POST['reg_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+
+
+    if (empty($username)) { array_push($errors, "Username is required"); }
+
+
+
+    $user_check_query = "SELECT * FROM users WHERE username='$username' LIMIT 1";
+    $result = mysqli_query($db, $user_check_query);
+    $user = mysqli_fetch_assoc($result);
+
+    if ($user) { // if user exists
+        if ($user['username'] === $username) {
+            array_push($errors, "Username already exists");
+            echo "Username already exists";
+        }
+    }
+
+    if (count($errors) == 0) {
+
+        $query = "INSERT INTO users (username) VALUES('$username')";
+        mysqli_query($db, $query);
+        $_SESSION['username'] = $username;
+        $_SESSION['success'] = "You are now logged in";
+        header('location: dashboard.php');
+    }
+}
+
+//LogIn Using Username
+if (isset($_POST['login_user'])) {
+    $username = mysqli_real_escape_string($db, $_POST['username']);
+
+    if (empty($username)) {
+        array_push($errors, "Username is required");
+    }
+
+    if (count($errors) == 0) {
+        $query = "SELECT * FROM users WHERE username='$username' ";
+        $results = mysqli_query($db, $query);
+        if (mysqli_num_rows($results) == 1) {
+            $_SESSION['username'] = $username;
+            $_SESSION['success'] = "You are now logged in";
+            header('location: dashboard.php');
+        }else {
+            array_push($errors, "Wrong username/password combination");
+        }
+    }
+}
+
+    
+//
+
+
+
+if (isset($_POST['submit'])) {
+
+    $topic=$_POST['topic'];
+    echo $topic;
+    $_SESSION['topic'] = $topic;
+    $textareaValue = trim($_POST['content']);
+    $_SESSION['textareaValue'] =  $textareaValue;
+    header('location: test.php');
+}
+
+
+$sum=0;
+$i=0;
+$percentage=0;
+if(isset($_POST["level"]))
+{
+    // Check if any option is selected
+    if(isset($_POST["subject"]))
+    {
+        // Retrieving each selected option
+
+        foreach ($_POST['subject'] as $subject)
+            $sum=$sum+$subject;
+
+
+        foreach ($_POST['subject'] as $subject)
+            $i=$i+1;
+
+
+
+        $percentage=($sum/($i*4))*100;
+        echo $percentage;
+        $_SESSION['percentage']=$percentage;
+
+    }
+    else
+        echo "Select an option first !!";
+}
+
+?>
+
+
